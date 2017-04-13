171ad0ef9000ba892ee0e310776b81e4faab3c5b
amymb <abertken@pivotal.io>
2017-04-13 13:02:06 -0600

    add https transport method

--- ci/tasks/reroute-green.sh
+++ ci/tasks/reroute-green.sh
@@ -3,6 +3,7 @@
 set -ex
 
 apt-get update && apt-get install -y wget
+apt-get install apt-transport-https
 apt-get install sudo
 wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
 echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
