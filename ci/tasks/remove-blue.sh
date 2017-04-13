099643a8e35286e9f9a508f566549dc0855175b2
amymb <abertken@pivotal.io>
2017-04-13 12:06:11 -0600

    change the way cf cli is installed

--- ci/tasks/remove-blue.sh
+++ ci/tasks/remove-blue.sh
@@ -2,11 +2,17 @@
 
 set -ex
 
-wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
-echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
-
-sudo apt-get update
-sudo apt-get install cf-cli
+apt-get update && apt-get install -y curl
+# ...download & extract Mac OS X binary
+curl -L "https://cli.run.pivotal.io/stable?release=macosx64-binary&source=github" | tar -zx
+# ...or Linux 64-bit binary
+curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
+# ...move it to /usr/local/bin or a location you know is in your $PATH
+mv cf /usr/local/bin
+# ...copy tab completion file on Ubuntu (takes affect after re-opening your shell)
+sudo curl -o /usr/share/bash-completion/completions/cf https://raw.githubusercontent.com/cloudfoundry/cli/master/ci/installers/completion/cf
+# ...and to confirm your cf CLI version
+cf --version
 
 cf login -a api.run.pivotal.io -u $USERNAME -p $PASSWORD -o Pivotal-Enablement -s staging --skip-ssl-certification
 
